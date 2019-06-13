90d1a7864d49e87e1b8c0a491dfa4ce823830b8b
Jianfeng Tan <henry.tjf@antfin.com>
2019-06-13 15:14:12 +0800

    dpdk: move to v18.11 as default DPDK version
    DPDK v18.11 is the latest LTS verison.
    
    As of the API/ABI changes introduced in ether at v18.08, we cannot
    compile l4fwd and nginx with old DPDK versions.
    
    Change-Id: I225302d9a257e9bce4aa22ff84d76a57170e7eb7
    Signed-off-by: Jianfeng Tan <henry.tjf@antfin.com>

--- examples/l4fwd/port.h
+++ examples/l4fwd/port.h
@@ -180,15 +180,17 @@ port_init(struct netbe_port *uprt, uint32_t proto)
 	if ((uprt->rx_offload & RX_CSUM_OFFLOAD) != 0) {
 		RTE_LOG(ERR, USER1, "%s(%u): enabling RX csum offload;\n",
 			__func__, uprt->id);
-		port_conf.rxmode.hw_ip_checksum = 1;
+		port_conf.rxmode.offloads |= uprt->rx_offload & RX_CSUM_OFFLOAD;
 	}
 	port_conf.rxmode.max_rx_pkt_len = uprt->mtu + ETHER_CRC_LEN;
 	if (port_conf.rxmode.max_rx_pkt_len > ETHER_MAX_LEN)
-		port_conf.rxmode.jumbo_frame = 1;
+		port_conf.rxmode.offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
 
 	rc = update_rss_conf(uprt, &dev_info, &port_conf, proto);
 	if (rc != 0)
 		return rc;
+
+	port_conf.txmode.offloads = uprt->tx_offload;
 
 	rc = rte_eth_dev_configure(uprt->id, uprt->nb_lcore, uprt->nb_lcore,
 			&port_conf);
@@ -220,11 +222,6 @@ queue_init(struct netbe_port *uprt, struct rte_mempool *mp)
 	nb_txd = RTE_MIN(TX_RING_SIZE, dev_info.tx_desc_lim.nb_max);
 
 	dev_info.default_txconf.tx_free_thresh = nb_txd / 2;
-	if (uprt->tx_offload != 0) {
-		RTE_LOG(ERR, USER1, "%s(%u): enabling full featured TX;\n",
-			__func__, uprt->id);
-		dev_info.default_txconf.txq_flags = 0;
-	}
 
 	for (q = 0; q < uprt->nb_lcore; q++) {
 		rc = rte_eth_rx_queue_setup(uprt->id, q, nb_rxd,
